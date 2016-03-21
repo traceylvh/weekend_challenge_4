341c0ea8d3ad6ea549a68192c59a923bd2ac0a44
tracey van haaften <tracey.lee.213@gmail.com>
2016-03-20 21:31:26 -0500

    updated for heroku

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -12,6 +12,33 @@ if (process.env.DATABASE_URL) {
   connectionString = 'postgres://localhost:5432/todo_db';
 }
 
+//create schema code for heroku
+
+pg.connect(connectionString, function(err, client, done){
+  if (err) {
+    console.log('Error connecting to DB!', err);
+    //TODO end process with error code
+  } else {
+    var query = client.query('CREATE TABLE IF NOT EXISTS todo_list (' +
+    'id SERIAL PRIMARY KEY,' +
+    'task varchar NOT NULL,' +
+    'status boolean DEFAULT TRUE);'
+  );
+
+  query.on('end', function(){
+    console.log('Successfully ensured schema exists');
+    done();
+  });
+
+  query.on('error', function() {
+    console.log('Error creating schema!');
+    //TODO exit(1)
+    done();
+  });
+}
+});
+
+
 
 
 //send list data back to front end
